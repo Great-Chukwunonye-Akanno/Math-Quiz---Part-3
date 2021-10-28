6e78728258a3eab0cc6b0e284b895fa6214030c2
Great-Chukwunonye-Akanno <83137882+Great-Chukwunonye-Akanno@users.noreply.github.com>
2021-10-28 16:52:49 -0600

    Add files via upload

---
+++ quiz_game_page.js
@@ -0,0 +1,6 @@
+function send()
+{
+    number_1 = document.getElementById("number_1").value;
+    number_2 = document.getElementById("number_2").value;
+    actual_answer = parseInt(number1) * parseInt(number_2)
+}
